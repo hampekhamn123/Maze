0c8609735931f18944c1880c094f0dd11798ed93
Hampus <hampus.andersson@elev.ga.lbs.se>
2018-10-19 15:33:16 +0200

    Animations scen

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+
+    public Animator cubeAnimation;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cubeAnimation.SetTrigger("Test");
+        }
+    }
+}
